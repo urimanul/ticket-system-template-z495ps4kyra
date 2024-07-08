96819fce32ca0087ec70a7f0033b140b2c68542f
urimanul <myopera9@gmail.com>
2024-07-09 05:15:45 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -93,11 +93,11 @@ if submitted:
 
     ticketNo = f"TICKET-{recent_ticket_number+1}"
     sql = "INSERT INTO support_ticket (ID,Issue,Status,Priority,Date_Submitted) values (ticketNo, issue,'オープン',priority,today);"
-    st.write(sql)
+    #st.write(sql)
     #sql = "INSERT INTO todo_tasks (Task_Subject) VALUES('xyzss')"
-    #cur.execute(sql)
+    cur.execute(sql)
 
-    #conn.commit()
+    conn.commit()
 
     # Show a little success message.
     st.write("チケットが提出されました。")
